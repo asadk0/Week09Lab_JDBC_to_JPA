8d342babbe0d8b1ee0dfaea77ec9440882d0caf5
759175 <759175@SAIT224378IT.ACDM.DS.SAIT.CA>
2020-12-05 18:23:41 -0700

    Lab 09 - JDBC to JPA

---
+++ src/java/database/UserDB.java
@@ -0,0 +1,86 @@
+package database;
+
+import models.Users;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class UserDB {
+
+    public int insert(Users user) throws NotesDBException {
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+       EntityTransaction trans = em.getTransaction();
+        try {
+           trans.begin();
+           em.persist(user);
+           em.merge(user);
+           trans.commit();
+        } catch (Exception ex) {
+           trans.rollback();
+        } finally {
+           em.close();
+        }
+        return 0;
+    }
+
+    public int update(Users user) throws NotesDBException {
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+       EntityTransaction trans = em.getTransaction();
+
+        try {
+           trans.begin();
+           em.merge(user);
+           trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+        return 0;
+}
+
+    public List<Users> getAll() throws NotesDBException {
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+           List<Users> users = em.createNamedQuery("Users.findAll", Users.class).getResultList();
+            return users;
+        } finally {
+           em.close();
+        }
+    }
+
+    /**
+     * Get a single user by their username.
+     *
+     * @param username The unique username.
+     * @return A User object if found, null otherwise.
+     * @throws NotesDBException
+     */
+    public Users getUser(String username) throws NotesDBException {
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+           Users user = em.find(Users.class, username);
+           return user;   
+        } finally {
+            em.close();
+            }
+    }
+
+    public int delete(Users user) throws NotesDBException {
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+       EntityTransaction trans = em.getTransaction();
+
+        try {
+           trans.begin();
+           em.remove(em.merge(user));
+           em.merge(user);
+           trans.commit(); 
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+           em.close();
+        }
+        return 0;
+    }
+}
